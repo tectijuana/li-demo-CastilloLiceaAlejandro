12a0ea11152ee327556d4ca1a4050e436d00079d
CastilloLiceaAlejandro <56612932+CastilloLiceaAlejandro@users.noreply.github.com>
2020-02-03 18:28:53 -0800

    Create 22.Suma_de_N_enteros.cpp

---
+++ 22.Suma_de_N_enteros.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1,n2;
+    int sum;
+    while(true){
+        cout<<"Numero 1: ";
+        cin>>n1;
+        cout<<"Numero 2: ";
+        cin>>n2;
+        sum=n1+n2;
+        cout<<n1<<" + "<<n2<<" = "<<sum<<endl;
+    }
+    return 0;
+}
